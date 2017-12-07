b872a4a6a63be71186f450c3966decbe1c78ae47
BobtheKebab <ahbab.ashraf@gmail.com>
2017-12-06 22:37:29 -0500

    Done

--- HW14.c
+++ HW14.c
@@ -5,24 +5,40 @@
 #include <sys/sem.h>
 #include <string.h>
 
+#define KEY 13
+
 int main (int argc, char* argv[]) {
 
   int semid;
 
   // Make sem and set val
   if (!strcmp(argv[1], "-c")) {
-    semid = semget(13, 1, IPC_CREAT);
-    semctl(semid, 1, SETVAL, argv[2]);
-    printf("nutt\n");
+    if (!argv[2]) {
+      printf("Wrong number of args mate\n");
+    } else {
+      semid = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+      if (semid == -1) {
+	printf("Semaphore already exists\n");
+      } else {
+	int temp;
+	sscanf(argv[2], "%d", &temp); // Turn char into int
+	semctl(semid, 0, SETVAL, temp);
+	printf("Semaphore created: %d\n", semid);
+	printf("Value set: %d\n", temp);
+      }
+    }
   }
 
   // Get val
   if (!strcmp(argv[1], "-v")) {
-    
+    semid = semget(KEY, 0, 0);
+    printf("Semphore value: %d\n", semctl(semid, 0, GETVAL));
   }
 
+  // Remove sem
   if (!strcmp(argv[1], "-r")) {
-    
+    semid = semget(KEY, 0, 0);
+    printf("Semaphore removed: %d\n", semctl(semid, 0, IPC_RMID));
   }
   
 }
